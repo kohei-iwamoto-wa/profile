bbd55fc52d767201b1b63d329b4114a7d701193d
kohei-iwamoto-wa <bananadesu9@gmail.com>
2019-03-26 06:58:26 +0900

    CorrectDetail

--- src/Top.js
+++ src/Top.js
@@ -4,7 +4,7 @@ class App extends Component {
   render(){
    return(
      <div className="top-image">
-       <span>profile  site</span>
+       <div className="title-message">profile  site</div>
      </div>
     );
   }
--- src/Works.js
+++ src/Works.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import book from './book.png';
+import portfolio from './portfolio.png';
 
 class App extends Component {
   render(){
-    
+
    return(
      <div className="profile-page">
        <h3>Works</h3>
@@ -13,6 +14,15 @@ class App extends Component {
            <img src={book}className="imageSize"></img>
            <div className="work-box__description">
              javascriptとGoogleBooksApiを用いて作成しました。
+           </div>
+         </div>
+       </div>
+
+       <div className="work-box">
+         <div className="work-box__thumbnail">
+           <img src={portfolio}className="imageSize"></img>
+           <div className="work-box__description">
+             React.jsで作成したポートフォリオサイト。シングルアプリケーションになってます。
            </div>
          </div>
        </div>

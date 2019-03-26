1f203809fa8a0302788a5978c42c1a8b18bb66c8
kohei-iwamoto-wa <bananadesu9@gmail.com>
2019-03-26 20:41:34 +0900

    Diffdelete

--- src/Top.js
+++ src/Top.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  withRouter
-} from 'react-router-dom';
-
-import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 class App extends Component {
   render(){
    return(
-     <TransitionGroup>
-       <CSSTransition
-       in
-       className="top-image"
-       apper={true}
-      >
      <div className="top-image">
        <div className="title-message">profile  site</div>
      </div>
-    </CSSTransition>
-  </TransitionGroup>
     );
   }
 }

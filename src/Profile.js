227517f6c866d70395f93afeec4c61c837a9f3fe
kohei-iwamoto-wa <bananadesu9@gmail.com>
2019-06-28 23:50:21 +0900

    profile編集

--- src/Profile.js
+++ src/Profile.js
@@ -18,6 +18,8 @@ class App extends Component {
          <p>2014年大学卒業　専攻は福祉</p>
          <p>2014年〜病院の事務員として働く</p>
          <p>2019年2月退職</p>
+         <p>2019年4月よりWEB制作会社で勤務</p>
+         <p>同年7月2日退職</p>
        </div>
       <div className="seikaku">
         <h3>Personality</h3>
